9ec012157e5ec3f1d1c30555dcf4bff3a406cece
Avlasova Kateryna <demix11jr@gmail.com>
2025-10-27 19:14:20 +0200

    test: add unit tests for NetSdrClient

--- NetSdrClientAppTests/NetSdrClientTests.cs
+++ NetSdrClientAppTests/NetSdrClientTests.cs
@@ -116,4 +116,82 @@ public class NetSdrClientTests
     }
 
     //TODO: cover the rest of the NetSdrClient code here
+    [Test]
+    public async Task ChangeFrequencyAsync_SendsCorrectMessage()
+    {
+        // Arrange
+        await _client.ConnectAsync();
+        long frequency = 145000000;
+        int channel = 1;
+
+        // Act
+        await _client.ChangeFrequencyAsync(frequency, channel);
+
+        // Assert
+        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()));
+        _tcpMock.VerifyGet(tcp => tcp.Connected);
+    }
+
+    [Test]
+    public async Task SendTcpRequest_NoConnection_ReturnsNull()
+    {
+        // Arrange
+        var message = new byte[] { 0x01, 0x02 };
+
+        var method = typeof(NetSdrClient)
+            .GetMethod("SendTcpRequest", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+
+        var taskObj = method.Invoke(_client, new object[] { message });
+        var task = (Task<byte[]>)taskObj;
+
+        var response = await task;
+
+        // Assert
+        Assert.IsNull(response);
+    }
+
+
+    [Test]
+    public async Task TcpClient_MessageReceived_CompletesTask()
+    {
+        // Arrange
+        _tcpMock = new Mock<ITcpClient>();
+        _tcpMock.Setup(t => t.Connected).Returns(true);
+
+        // без автоподії
+        _updMock = new Mock<IUdpClient>();
+        _client = new NetSdrClient(_tcpMock.Object, _updMock.Object);
+
+        var msg = new byte[] { 0x10, 0x20 };
+        var response = new byte[] { 0xAB, 0xCD };
+
+        var method = typeof(NetSdrClient)
+            .GetMethod("SendTcpRequest", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+        // Act
+        var taskObj = method.Invoke(_client, new object[] { msg });
+        var task = (Task<byte[]>)taskObj;
+        _tcpMock.Raise(t => t.MessageReceived += null, _tcpMock.Object, response);
+
+        var result = await task;
+
+        // Assert
+        Assert.That(result, Is.EqualTo(response));
+    }
+
+    [Test]
+    public void TcpClient_MessageReceived_NoPendingRequest_DoesNotThrow()
+    {
+        // Arrange
+        var msg = new byte[] { 0xAA, 0xBB };
+
+        // Act & Assert
+        Assert.DoesNotThrow(() =>
+            _tcpMock.Raise(t => t.MessageReceived += null, _tcpMock.Object, msg)
+        );
+    }
+
+
 }
