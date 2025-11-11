0a1ab70c7c7abad317fb6b2bc1111407b8aad08f
Avlasova Kateryna <demix11jr@gmail.com>
2025-11-11 18:20:54 +0200

    refactor: transfer general logic to new methods for UpdClientWrapper and TcpClientWrapper

--- NetSdrClientApp/Networking/UdpClientWrapper.cs
+++ NetSdrClientApp/Networking/UdpClientWrapper.cs
@@ -49,19 +49,15 @@ namespace NetSdrClientApp.Networking
 
         public void StopListening()
         {
-            try
-            {
-                _cts?.Cancel();
-                _udpClient?.Close();
-                Console.WriteLine("Stopped listening for UDP messages.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error while stopping: {ex.Message}");
-            }
+            StopInternal();
         }
 
         public void Exit()
+        {
+            StopInternal();
+        }
+
+        private void StopInternal()
         {
             try
             {
@@ -74,7 +70,6 @@ namespace NetSdrClientApp.Networking
                 Console.WriteLine($"Error while stopping: {ex.Message}");
             }
         }
-
         public override int GetHashCode()
         {
             var payload = $"{nameof(UdpClientWrapper)}|{_localEndPoint.Address}|{_localEndPoint.Port}";
