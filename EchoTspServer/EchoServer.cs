0a98582665e46478f9c9f0b35aff6e851e0a6fb1
Avlasova Kateryna <demix11jr@gmail.com>
2025-11-20 18:13:41 +0200

    refactor: Change the 'ReadAsync' method call to use the 'Stream.ReadAsync(Memory<byte>, CancellationToken)' overload

--- EchoTspServer/EchoServer.cs
+++ EchoTspServer/EchoServer.cs
@@ -54,10 +54,10 @@ namespace EchoServer
                     byte[] buffer = new byte[8192];
                     int bytesRead;
 
-                    while (!token.IsCancellationRequested && (bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, token)) > 0)
+                    while (!token.IsCancellationRequested && (bytesRead = await stream.ReadAsync(buffer.AsMemory(0, buffer.Length), token)) > 0)
                     {
                         // Echo back the received message
-                        await stream.WriteAsync(buffer, 0, bytesRead, token);
+                        await stream.WriteAsync(buffer.AsMemory(0, bytesRead), token);
                         Console.WriteLine($"Echoed {bytesRead} bytes to the client.");
                     }
                 }
