e92e7e4a36a2350e297b14b89d8e8554bfe0d94c
Avlasova Kateryna <demix11jr@gmail.com>
2025-11-21 14:44:19 +0200

    refactor: make Class Program static

--- EchoTspServer/Program.cs
+++ EchoTspServer/Program.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace EchoServer
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
